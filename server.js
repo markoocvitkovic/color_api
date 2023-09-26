86d5b2402b4f2403361628da8b9c4c1a8597b97e
markoocvitkovic <115668419+markoocvitkovic@users.noreply.github.com>
2023-09-26 18:57:55 +0200

    first

---
+++ server.js
@@ -0,0 +1,17 @@
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults();
+const port = process.env.PORT || 3000;
+
+server.use(middlewares);
+server.use(router);
+
+server.listen(port);
+
+router.render = (req, res) => {
+  res.jsonp({
+    color: res.locals.data
+  })
+}
+
